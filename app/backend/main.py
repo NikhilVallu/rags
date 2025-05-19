e96a6ca5811c7cfd43a3973e1e80d448c269b6ac
Nikhil Vallu <nikhilvallu@Nikhils-MacBook-Pro.local>
2025-05-19 14:48:44 -0400

    Initial commit

---
+++ app/backend/main.py
@@ -0,0 +1,15 @@
+import os
+
+from app import create_app
+from load_azd_env import load_azd_env
+
+# WEBSITE_HOSTNAME is always set by App Service, RUNNING_IN_PRODUCTION is set in main.bicep
+RUNNING_ON_AZURE = os.getenv("WEBSITE_HOSTNAME") is not None or os.getenv("RUNNING_IN_PRODUCTION") is not None
+
+if not RUNNING_ON_AZURE:
+    load_azd_env()
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
